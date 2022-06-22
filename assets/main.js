a0567828fccd8dfa55514fc7a5a2b1a744bed301
user1 <kiokoerick040@gmail.com>
2022-06-22 19:47:36 +0300

    last commit

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+function welcome() {
+    const inputValue = document.querySelector("input").value;
+    const generatedElement = document.createElement("p");
+    const text = document.createTextNode(`Hello ${inputValue}, welcome to the bootcamp`);
+    generatedElement.appendChild(text);
+    document.body.appendChild(generatedElement);
+} 
